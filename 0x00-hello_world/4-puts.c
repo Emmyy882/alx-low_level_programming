4337b579f5440bcdf9ad178e83b2b8a79b999002
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-16 18:24:11 +0100

    program to output a set of strings using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * Return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
-{    
-
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
-
 	puts(str);
-
 	return (0);
-
 }
