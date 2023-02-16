3c012c09231268420bc953d5cbaaebc0d30908dd
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-16 22:25:37 +0100

    program to print sizeof datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int c;
 	long long int b;
 	float d;
+
 	printf("Size of a char: %lu", sizeof(a), " byte (s)\n");
 	printf("Size of an int: %lu", sizeof(txt), " byte (s)\n");
 	printf("Size of a long int: %lu", sizeof(c), " byte (s)\n");
