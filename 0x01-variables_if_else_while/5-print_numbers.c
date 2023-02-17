5e44b2f717015e8389030ef4072eb8d054f1fa13
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-18 00:52:27 +0100

    program to print numbers from 0 - 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * program to print numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i, "\n");
+	}
+	return (0);
+}
