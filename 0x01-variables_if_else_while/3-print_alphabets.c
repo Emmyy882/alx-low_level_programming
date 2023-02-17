057ae295d126bcae98588e77c37a65074cf1063c
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-18 00:04:36 +0100

    printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * program to output the letters of the alphabet from small letters to capital letters using the putchar() function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('$');
+	putchar('\n');
+}
