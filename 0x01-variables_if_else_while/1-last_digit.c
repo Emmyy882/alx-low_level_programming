0f77577977307f6e30d1f47c0824a867fce46a85
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-18 00:19:00 +0100

    printing the last digit of a set of random numbers assigned to a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
-	if (lastDigit > 5)
+	lastDigt = n % 10;
+	if (lastDigt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigt);
 	}
-	else if (lastDigit == 0)
+	else if (lastDigt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastDigt);
 	}
-	else if (lastDigit < 6 && lastDigit != 0)
+	else if (lastDigt < 6 && lastDigt != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigt);
 	}
 	return (0);
 }
