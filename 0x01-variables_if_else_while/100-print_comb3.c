2d1a8d3abf4e6e1a5a1e320b15ba697c976469ba
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-18 02:31:21 +0100

    printing numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	int ones = '0';
-	int tens ='0';
+	int tens = '0';
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = '0'; ones <= 9; ones++)
 		{
-			if (!((ones = tens) || (tens > ones)))
+			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
