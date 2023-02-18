11bb241f5757ad6c3d02cd430d7f65782b4236f7
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-18 01:37:08 +0100

    printing numbers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * program that prints lowercase in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while
+		(c > 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar("\n");
+	return (0);
+}
