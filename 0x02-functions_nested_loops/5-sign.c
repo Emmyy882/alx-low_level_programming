567d3e03b68f917b80152f8052368ba7cd42c25a
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-21 18:34:59 +0100

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * n - is the parameter to store the number passed
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(0);
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
