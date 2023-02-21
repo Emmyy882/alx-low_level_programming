634200242b847b36569bc86957cfb2b8ed4ac28c
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-21 23:10:10 +0100

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs(int) - function to print absolute number
+ * int - parameter 
+ */
+int _abs(int)
+{
+	int absolute;
+
+	absolute = abs(int);
+	return absolute;
+}
