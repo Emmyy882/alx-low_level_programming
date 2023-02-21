33ceb67e0b3caa4d0747c4e0197762629343391f
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-21 17:59:22 +0100

    upgrade

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check letters if lowercase or uppercase
+ * Return: 1 if true otherwise 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
