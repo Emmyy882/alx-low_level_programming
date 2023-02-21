f89c1053fa3051d07a4482086f0304b93330cdec
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-21 15:35:17 +0100

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print alphabet in small letter
+ * printing small letters
+ * print_alphabet to print small letter
  * Result: Always 0 (Sucess)
  */
 void print_alphabet(void)
