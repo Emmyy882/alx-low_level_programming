32fb0706cd9fede26362f6fc0145bb976dd315b9
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-22 02:40:40 +0100

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Takes the parameter
+ * @n: The parameter variable
+ */
+void print_to_98(int n)
+{
+	if (n >= 0 && n <= 98)
+	{
+		for (
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,4 +5,13 @@
  */
 void times_table(void)
 {
-	
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d, ", i * j);
+		}
+	}
+}
