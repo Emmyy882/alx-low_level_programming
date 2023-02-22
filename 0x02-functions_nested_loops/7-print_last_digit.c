7294dd753bfc1b40742ed247e0d07aa989596186
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-22 01:37:29 +0100

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,12 @@
 
 /**
  * print_last_digit() - print last digit of number
- * Return: the last digit
+ * @n: The number
  */
 int print_last_digit(int n)
 {
-	
+	int last_digit;
+
+	last_digit = n % 10;
+	return (last_digit);
+}
