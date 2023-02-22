4f9a869ccd374f2fc9ecab05fb907495241abf0b
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-22 02:14:58 +0100

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Takes in the parameters to be added
+ * @a: The first parameter
+ * @b: The second parameter
+ * Return: result of operation
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
