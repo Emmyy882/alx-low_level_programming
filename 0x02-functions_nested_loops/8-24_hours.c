fe6065d08ac34817368f1f15ab3af433219d5e27
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-22 10:46:29 +0100

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,27 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i, j, k, m;
 
-	for (i = 0; i <= 23; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 59; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			printf("%d:%d\n", i, j);
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+					_putchar('\n');
+				}
+			}
 		}
 	}
 }
