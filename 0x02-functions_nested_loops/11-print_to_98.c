6e380d7ce940bbb29890f75750790ffc2d7433fb
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-22 03:20:10 +0100

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,4 +9,23 @@ void print_to_98(int n)
 {
 	if (n >= 0 && n <= 98)
 	{
-		for (
+		for (n; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (n; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 0)
+	{
+		for (n; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+}
