eaeed407d9f43bcc94e2e7c8382d7ea422fbccf4
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-22 10:48:58 +0100

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,28 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int a, b, op;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
+
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
-			printf("%d, ", i * j);
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
 		}
+		_putchar('\n');
 	}
 }
