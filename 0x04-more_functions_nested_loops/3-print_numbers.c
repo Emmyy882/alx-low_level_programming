44ad1f436ab5463a03331e0bb7c29f4439b90d8f
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-23 21:37:27 +0100

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 - 9
+ * Return: The numbers since 0 up to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
