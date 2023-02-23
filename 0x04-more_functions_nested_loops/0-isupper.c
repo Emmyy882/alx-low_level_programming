359534065c5ffed949ad9665de15a098845164b1
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-23 14:39:52 +0100

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,12 @@
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
-int _isupper(int x)
+int _isupper(int c)
 {
-        if (x >= 65 && x <= 90)
-        {
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-        }
+	}
 	else
 	{
 		return (0);
