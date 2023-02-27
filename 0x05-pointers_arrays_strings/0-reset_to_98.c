57bb2a1307fdd8bad0b394334549ec6e09aa75fd
Emmyy882 <emmaagbaje3@gmail.com>
2023-02-27 12:03:22 +0100

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,10 @@
 
 /**
  * reset_to_98 - Takes in a pointer variable
- * @*n: pointer variable
+ * @n: pointer variable
  * Return; empty
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	_putchar('\n');
 }
