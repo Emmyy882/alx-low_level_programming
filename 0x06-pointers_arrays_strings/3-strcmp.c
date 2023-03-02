4ca01c5c8d45a7cc8d9b04105beff81523db1717
Emmyy882 <emmaagbaje3@gmail.com>
2023-03-02 18:23:12 +0100

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,22 @@
 #include "main.h"
-
 /**
- * _strcmp - compare strings
- * @s1: string 1
- * @s2: string 2
- * Return: result of compared strings
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i = 0, op = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (op == 0)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
 		i++;
 	}
-	return (0);
+
+	return (op);
 }
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - converts string to uppercase
+ * @str: parameter
+ * Return: uppercase
+ */
+char *string_toupper(char *)
+{
+
