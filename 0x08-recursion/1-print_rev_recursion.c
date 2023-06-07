2917358972fa51a0ef5e014ab630c14e2c24e156
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-07 21:03:37 +0100

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * _print_rev_recursion: prints the reverse of a string
+ * _print_rev_recursion - prints the reverse of a string
  * @s: the string
  * Return: 0 returns string in reverse order
  */
@@ -9,8 +9,8 @@ void _print_rev_recursion(char *s)
 	if (*s == '\0')
 	{
 		return;
+		_putchar('\n');
 	}
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	_putchar('\n');
 }
