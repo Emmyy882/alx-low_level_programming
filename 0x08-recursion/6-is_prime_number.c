b216014f23acdaeb8282796ef8e5269e5f492a67
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-08 09:48:11 +0100

    update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_number - checks is a number is prime
+ * @n: the number
+ * Return: 1 if prime or 0 if not prime
+ */
+int isPrimeRecursive(int n, int divisor)
+{
+    if (n <= 1)
+    {
+        return (0);
+    }
+    if (divisor == 1)
+    {
+        return (1);
+    }
+    if (n % divisor == 0)
+    {
+        return (0);
+    }
+    return isPrimeRecursive(n, divisor - 1);
+}
+
+int is_prime_number(int n)
+{
+    return isPrimeRecursive(n, n - 1);
+}
+
