4798dad17a030feb293382e834c1c58a90c9b771
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-09 19:56:13 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "main.h"
 /**
  * main - receives arguments
  * @argc: number of arguments passed to function
@@ -9,17 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
 	printf("%s\n", *argv);
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s", argv[i]);
-		}
-		_putchar('\n');
-	}
-	_putchar('\n');
+
 	return (0);
 }
