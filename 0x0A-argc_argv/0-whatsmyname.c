985929f4b283d63e062e6aa7bf651fa0063edf42
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-09 19:43:37 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "main.h"
-
+/**
+ * main - receives arguments
+ * @argc: number of arguments passed to function
+ * @argv: array of string arguments
+ * Return: the arguments passed
+ */
 int main(int argc, char *argv[])
 {
 	int i;
