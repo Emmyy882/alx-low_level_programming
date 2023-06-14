c0cdd5e77ca890bef00f39350277d8464fd3021b
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-14 09:58:20 +0100

    update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strcpy - make a copy of passed string
+ * @dest: where to copy string to
+ * @str: the string variable
+ * Return: copy of str
+ */
+char *_strcpy(char *dest, char *str)
+{
+        int i;
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                dest[i] = str[i];
+        }
+        dest[i++] = '\0';
+        return (dest);
+}
+
+/**
+ * _strlen - gets lenght of string
+ * @src: the string
+ * return: lenght of string
+ */
+int _strlen(char *src)
+{
+	int len;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strdup - makes duplicate of a string
+ * @str: the source string
+ * Return: copy of string
+ */
+char *_strdup(char *str)
+{
+	unsigned int len;
+	char *copy;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = _strlen(str);
+	copy = (char *)malloc((len + 1) * sizeof(char));
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	copy = _strcpy(copy, str);
+	return (copy);
+}
+
+	
