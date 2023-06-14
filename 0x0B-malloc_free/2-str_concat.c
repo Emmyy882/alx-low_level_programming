2085ecd0a47e6cf4f8fb1722e88984852b2c8771
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-14 23:39:44 +0100

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strcat - adds two strings together
+ * @s1: first string
+ * @s2: second string
+ * Return: concat of two strings
+ */
+char *_strcat(char *newstr, char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+
+	while (s1[i] != '\0')
+	{
+		newstr[i] = s1[i];
+		i++;
+	}
+
+
+	while (s2[j] != '\0')
+	{
+		newstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
+
+/**
  * _strlen - gets length of strings
  * @s1: string one
  * @s2: string two
  * Return: length of both strings
  */
-int _strlen(char *s1, char *s2)
+int _strlen(char *s)
 {
 	int len;
-	int len1;
-	int len2;
 
-	len1 = 0;
-	while (s1[len1] != '\0')
+	len = 0;
+	while (s[len] != '\0')
 	{
-		len1++;
+		len++;
 	}
-	len2 = 0;
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	len2++;
-	len = len1 + len2;
 	return (len);
 }
 
@@ -35,32 +54,26 @@ int _strlen(char *s1, char *s2)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int j;
-	int len;
-	char *dest;
+	int len1, len2;
+	char *newstr;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	newstr = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (newstr == NULL)
 	{
 		return (NULL);
 	}
-	len = _strlen(s1, s2);
-	dest = (char *)malloc(len * sizeof(char));
-	for (i = 0; i < len; i++)
-	{
-		if (s1[i] != '\0')
-		{
-			dest[i] = s1[i];
-		}
-	}
-	dest[i++] = ' ';
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		dest[i] = s2[j];
-		i++;
-		j++;
-	}
-	dest[i++] = '\0';
-	return (dest);
+	newstr = _strcat(newstr, s1, s2);
+	return (newstr);
 }
