db33540675884f6e1e594abed34310b91d373c9e
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-15 19:11:05 +0100

    update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: passed argument
+ *
+ * Return: void 
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+
+	return (ptr);
+}
