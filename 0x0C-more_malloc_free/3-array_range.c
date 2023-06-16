88c5f75f71cd771c7792d9c8cc269100fbcc5e2b
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-16 13:14:18 +0100

    update

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	ptr = malloc(max * sizeof(int));
+	ptr = malloc((max - min + 1) * sizeof(int));
 	if (ptr == NULL)
 	{
 		return (NULL);
