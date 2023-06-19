71d1f00ca95092c6ff81337cbc28740270fa549b
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-19 23:54:12 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog - dog structure
+ * strucct dog - dog structure
  * @name: dog name
  * @age: dog age
  * @owner: name of dog owner
+ *
+ * Description: struct takes description os a dog
  */
 struct dog {
 	char *name;
