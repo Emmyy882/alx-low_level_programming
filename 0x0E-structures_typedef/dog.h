8949ff7ab739741d61671df11f71901bf2538ee1
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-19 23:50:29 +0100

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of dog owner
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
