2b23851ac5cfaef19b908de1afd43f5efcabe22a
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-20 01:42:59 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,13 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - prototype of function to initialize struct variables
+ * @d: dog object
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
