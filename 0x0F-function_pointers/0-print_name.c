f05aecc317d687851d62f2cb011933df6f925051
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-21 22:02:55 +0100

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - call back function to print name
+ * @name: parameter that holds passed value
+ * @f: pointer to function
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name); //callback function that f
+		 //points to
+}
