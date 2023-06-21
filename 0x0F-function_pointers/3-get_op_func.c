5625fbfab51095bea8fa3d80ebf08c601f9d6883
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-22 00:39:35 +0100

    update

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,18 @@
+#include "calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform the operation asked by the user
+ * @s: takes the operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * op_sub - returns the difference of a and b
+ * op_mul - returns the product of a and b
+ * op_div - returns the result of the division of a and b
+ * op_mod - returns the remainder of the division of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
