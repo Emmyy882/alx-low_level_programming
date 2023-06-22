64b356ca8112521374df0678623c2811b980b47b
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-22 23:39:11 +0100

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,6 +18,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		return;
 
+	if (n == 0)
+		printf("%d", 0);
+
 	va_start(args, n);
 	for (i = 0; i < size; i++)
 	{
