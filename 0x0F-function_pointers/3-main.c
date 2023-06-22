cea8f6a7b9fd7c4e27a4541480bea90a1f71dd24
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-22 14:13:40 +0100

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	result = get_op_func(argv[2])(num1, num2);
+	result = (*get_op_func(argv[2])(num1, num2));
 
 	return (result);
 }
