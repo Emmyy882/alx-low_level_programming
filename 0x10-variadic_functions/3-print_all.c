8799909ef7390bdb5bdd4d1e7e520adc04c53674
Emmyy882 <emmaagbaje3@gmail.com>
2023-06-23 10:22:30 +0100

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,4 +10,42 @@
 
 void print_all(const char * const format, ...)
 {
+	int i = 0, num = 0;
+	char c;
+	float f;
+	char *s;
+
+	va_list args;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == 'i')
+		{
+			num = va_arg(args, int);
+			printf("%d, ", num);
+		}
+		else if (format[i] == 'c')
+		{
+			c = (char)va_arg(args, int);
+			printf("%c, ", c);
+		}
+		else if (format[i] == 'f')
+		{
+			f = va_arg(args, double);
+			printf("%f, ", f);
+		}
+		else if (format[i] == 's')
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s, ", s);
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
+
 
