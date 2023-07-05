38f20283818a62fec3222202644d3202da311392
Emmyy882 <emmaagbaje3@gmail.com>
2023-07-05 14:01:46 +0100

    update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to pointer of list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
-
 size_t print_listint(const listint_t *h);
 
 size_t listint_len(const listint_t *h);
