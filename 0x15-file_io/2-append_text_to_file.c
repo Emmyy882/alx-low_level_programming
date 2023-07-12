e2e9d84d4f1a63650ef541344f4bc11ebb1a9a41
Emmyy882 <emmaagbaje3@gmail.com>
2023-07-12 06:56:26 +0100

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,10 +28,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
-
 	if (o == -1 || w == -1)
 		return (-1);
-
 	close(o);
+	
 	return (1);
 }
